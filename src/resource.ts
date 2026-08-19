5956798d8abe4201d6f919d35fe99b7d7684d6a1
Tony Benavides <marco.benavides@prizepicks.com>
2026-08-19 13:56:55 -0400

    chore(cf-poc): Added asset for existence of dispatch api token secret.

--- src/resource.ts
+++ src/resource.ts
@@ -18,7 +18,16 @@ const BaseURI = (env: Env) =>
 const ScriptsURI = (env: Env) =>
 	`${BaseURI(env)}/dispatch/namespaces/${env.DISPATCH_NAMESPACE_NAME}/scripts`;
 
+function assertDispatchToken(env: Env): void {
+	if (!env.DISPATCH_NAMESPACE_API_TOKEN) {
+		console.error(
+			"DISPATCH_NAMESPACE_API_TOKEN is not configured; dispatch namespace API calls will fail",
+		);
+	}
+}
+
 const AuthHeaders = (env: Env) => ({
+	assertDispatchToken(env);
 	Authorization: `Bearer ${env.DISPATCH_NAMESPACE_API_TOKEN}`,
 });
 
